85bd2b8129babd44de901555557be145d229f6cf
Medivh <ext.zhu.yang@badanamu.com.cn>
2020-10-23 15:03:26 +0800

    refactor: optimize default

--- snowflake/snowflake.go
+++ snowflake/snowflake.go
@@ -5,16 +5,12 @@ import "sync"
 var instance *Node
 var once = sync.Once{}
 
-func getDefault() *Node {
+func Generate() ID {
 	once.Do(func() {
 		var err error
 		if instance, err = NewNode(0); err != nil {
 			panic(err)
 		}
 	})
-	return instance
-}
-
-func Generate() ID {
-	return getDefault().Generate()
+	return instance.Generate()
 }
